8827b0ce1d3a246178b1cb9fe16aa234ccfba607
Umberto Sonnino <rimher@gmail.com>
2019-04-15 18:47:56 +0200

    Pass antliasing parameter from C

--- include/core/SkCanvas.h
+++ include/core/SkCanvas.h
@@ -1007,7 +1007,7 @@ public:
         @param path         SkPath to combine with clip
         @param doAntiAlias  true if clip is to be anti-aliased
     */
-    void clipPath(const SkPath& path, bool doAntiAlias = true) {
+    void clipPath(const SkPath& path, bool doAntiAlias = false) {
         this->clipPath(path, SkClipOp::kIntersect, doAntiAlias);
     }
 
